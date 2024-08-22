c4ff62a1dd8d3c1b64d086fdfe2109e5d16b2c85
Ashish-2458 <160274733+Ashish-2458@users.noreply.github.com>
2024-08-22 15:57:21 +0000

    data types summeries

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,4 @@
+const id = 12345
+let accountmail = "asha2gmail.com"
+
+console.table ([accountmail,id])
---
+++ 01_basics/data types summeries.js
@@ -0,0 +1,20 @@
+//  Premitive 
+
+// 7 types : String ,Number, Boolean, Null, Undefined, Symbol, BigInt
+
+//Reference (Non Premitive)
+
+// Array, Object, Functions
+
+const heroes = ["spiderman","ironman","Captain America"]
+
+let myObj ={
+    Name : "ashish",
+    age : 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof heroes)
